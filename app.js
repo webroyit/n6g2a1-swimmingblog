175fce8c3f52cbc7103559540be45555d2f6990b
royweb <xytank@gmail.com>
2020-06-22 14:43:20 -0400

    Set up routes

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
+// Routes
+app.use('/', require('./routes/index'));
+
 const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, console.log(`Server running in ${PORT}`));
