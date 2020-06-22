258aec4597c32a859a935452862b3fd89fd43f9d
royweb <xytank@gmail.com>
2020-06-22 12:39:34 -0400

    Use morgan for logging

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const connectDB = require('./config/db');
 
 connectDB();
 
 const app = express();
+
+// For logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 const PORT = process.env.PORT || 1000;
 
