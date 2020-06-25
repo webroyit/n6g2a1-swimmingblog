a01a4df4082c04d4f50bb2cd875b39da8ac9c75e
royweb <xytank@gmail.com>
2020-06-25 11:52:01 -0400

    Use google profile data to create a new user

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,27 @@ module.exports = function(passport){
     },
     // done is the callback
     async (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value
+        }
+
+        try{
+            let user = await User.findOne({ googleId: profile.id });
+
+            if(user){
+                // null for error
+                done(null, user);
+            }else{
+                user = await User.create(newUser);
+                done(null, user);
+            }
+        }catch(err){
+            console.error(err);
+        }
     }));
 
     // Determines which data of the user object should be stored in the session
