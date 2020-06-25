1d687d97c321b77c4a563eb892853c98267f624d
royweb <xytank@gmail.com>
2020-06-25 12:19:17 -0400

    Add a route to logout user

--- routes/auth.js
+++ routes/auth.js
@@ -12,4 +12,12 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
     res.redirect('/dashboard');
 });
 
+// @desc    Logout
+// @route   GET /auth/logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+
 module.exports = router;
