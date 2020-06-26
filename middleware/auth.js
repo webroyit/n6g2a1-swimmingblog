762677b5d40d426161e4c085cd01874fff9c87c5
royweb <xytank@gmail.com>
2020-06-26 12:28:21 -0400

    Add a middleware to redirect the user to dashboard page if they are login

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,3 @@
-const { response } = require("express");
-
 module.exports = {
     ensureAuth: function(req, res, next){
         if(req.isAuthenticated()){
@@ -7,6 +5,14 @@ module.exports = {
         } else{
             // Redirect the user to login page if they did not login
             res.redirect('/');
+        }
+    },
+    ensureGuest: function(req, res, next){
+        if(req.isAuthenticated()){
+            // Redirect the user to dashboard page if they are login
+            res.redirect('/dashboard');
+        } else{
+            return next();
         }
     }
 }
