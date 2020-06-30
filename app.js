fe5f807f5b6a4644ac36470511d4b5a6e7f77b2d
royweb <xytank@gmail.com>
2020-06-30 14:55:27 -0400

    Use helper method to format the date

--- app.js
+++ app.js
@@ -24,8 +24,11 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+// Handlebars Helpers
+const { formatDate } = require('./helpers/hbs');
+
 // Use Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
 // Express Sessions Middleware
--- helpers/hbs.js
+++ helpers/hbs.js
@@ -3,6 +3,6 @@ const moment = require('moment');
 
 module.exports = {
     formatDate: function (date, format){
-        return moment(date.format(format));
+        return moment(date).format(format);
     }
 }
