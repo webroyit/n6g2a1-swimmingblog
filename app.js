b1238b50c6a95177dd034e2adccd83a965bc0692
royweb <xytank@gmail.com>
2020-07-02 13:51:41 -0400

    Add a helper method to update the select input from the server

--- app.js
+++ app.js
@@ -25,14 +25,15 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 // Handlebars Helpers
-const { formatDate, truncate, stripTags, editIcon } = require('./helpers/hbs');
+const { formatDate, truncate, stripTags, editIcon, select } = require('./helpers/hbs');
 
 // Use Handlebars
 app.engine('.hbs', exphbs({helpers: {
     formatDate,
     truncate,
     stripTags,
-    editIcon
+    editIcon,
+    select
 },
     defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
--- helpers/hbs.js
+++ helpers/hbs.js
@@ -30,5 +30,18 @@ module.exports = {
         } else {
             return '';
         }
-      }
+    },
+    // For select element
+    select: function (selected, options) {
+        return options
+            .fn(this)
+            .replace(
+                new RegExp(' value="' + selected + '"'),
+                '$& selected="selected"'
+            )
+            .replace(
+                new RegExp('>' + selected + '</option>'),
+                ' selected="selected"$&'
+            )
+    }
 }
