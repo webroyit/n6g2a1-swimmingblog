270c9c2a8b93c9b3741429a1fedadf63d2880892
royweb <xytank@gmail.com>
2020-07-02 13:15:42 -0400

    Add edit story form page

--- routes/stories.js
+++ routes/stories.js
@@ -46,24 +46,28 @@ router.get('/', ensureAuth, async (req, res) => {
 // @desc    Show edit page
 // @route   GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-    const story = await Story.findOne({
-        _id: req.params.id
-    }).lean();
-
-    if(!story){
-        return res.res.render('error/404');
+    try{
+        const story = await Story.findOne({
+            _id: req.params.id
+        }).lean();
+    
+        if(!story){
+            return res.res.render('error/404');
+        }
+    
+        if(story.user != req.user.id){
+            res.redirect('stories');
+        }
+        else{
+            res.render('stories/edit', {
+                story
+            });
+        }
+    } catch(err){
+        console.error(err);
+        res.render('error/500');
     }
-
-    if(story.user != req.user.id){
-        res.redirect('stories');
-    }
-    else{
-        res.render('stories/edit', {
-            story
-        });
-    }
-
-    res.render('stories/add');
+    
 });
 
 module.exports = router;
